2176e006b7bcee0d0d91192cf3cde07c49c26512
mannyblunders <marcia.nunesribeiro@unifr.ch>
2024-07-26 13:23:09 +0200

    healthbar (part1)

--- Assets/scripts/health.cs
+++ Assets/scripts/health.cs
@@ -2,35 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class health : MonoBehaviour
+public class Health : MonoBehaviour
 {
     [SerializeField] private float startingHealth;
-    public float currentHealth { get; private set; }
-    private float damage;
+    public float currentHealth { get; private set; }//ony changed by me 
+    private Animator anim;
+    private bool dead;
 
     private void Awake()
     {
         currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
     }
 
     public void TakeDamage(float _damage)
     {
-        currentHealth = Mathf.Clamp(currentHealth - damage, 0, startingHealth);
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
 
         if (currentHealth < startingHealth)
         {
-            //player hurt
+            anim.SetTrigger("hurt");
         }
         else
         {
-            //player dead
+            if (dead)
+            {
+                anim.SetTrigger("die");
+                //GetComponent<PlayerMovement>().enabled = false;
+                dead = true;
+            }
         }
     }
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E)) ;
-        TakeDamage(1);
-    }
+   
 
 }
