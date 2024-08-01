4fbe12f434283f8aa83f21f996b0611b67cd7839
mannyblunders <marcia.nunesribeiro@unifr.ch>
2024-08-01 21:30:20 +0200

    scene 2 sophie

--- Assets/scripts/Dialogue/DIalogueAnimmator.cs
+++ Assets/scripts/Dialogue/DIalogueAnimmator.cs
@@ -6,18 +6,41 @@ public class DIalogueAnimmator : MonoBehaviour
 {
     //public Animator animator;
     public DialogueMAnager dm;
-
     public GameObject dialogButton;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (dialogButton != null)
+        {
+            dialogButton.SetActive(true);
+        }
+        else
+        {
+            Debug.LogWarning("dialogButton is null in OnTriggerEnter2D");
+        }
+
         //animator.SetBool("startOpen", true);
-        dialogButton.SetActive(true);
     }
     private void OnTriggerExit2D(Collider2D other)
     {
-        dialogButton.SetActive(false);
+        if (dialogButton != null)
+        {
+            dialogButton.SetActive(false);
+        }
+        else
+        {
+            Debug.LogWarning("dialogButton is null in OnTriggerExit2D");
+        }
+
+        if (dm != null)
+        {
+            dm.EndDialogue();
+        }
+        else
+        {
+            Debug.LogWarning("DialogueManager (dm) is null in OnTriggerExit2D");
+        }
+
         //animator.SetBool("startOpen", false);
-        dm.EndDialogue();
     }
 }
